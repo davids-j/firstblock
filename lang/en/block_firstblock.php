0a615ee285e7a5b56da14deb12be9d50a21e9c27
David <david@furfm.net>
2020-12-10 22:55:27 +0100

    first google search block

---
+++ lang/en/block_firstblock.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Simple HTML block';
+$string['firstblock'] = 'Simple HTML';
+$string['firstblock:addinstance'] = 'Add a new simple HTML block';
+$string['firstblock:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
